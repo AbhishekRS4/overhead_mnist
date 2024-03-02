94aa1e5cadf871c30cc7b4b16bd3b1c5162d30f2
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2024-03-02 15:44:38 +0100

    added script to save label mapping to json

---
+++ modeling/gen_label_mapping_json.py
@@ -0,0 +1,36 @@
+import os
+import json
+import argparse
+
+
+def gen_label_mapping_json(ARGS):
+    file_json = "label_mapping.json"
+    list_labels = sorted(os.listdir(ARGS.dir_train))
+    num_labels = len(list_labels)
+    dict_label_mapping = {}
+    for lbl_idx in range(num_labels):
+        dict_label_mapping[lbl_idx] = list_labels[lbl_idx]
+    with open(file_json, "w", encoding="utf-8") as file_des:
+        json.dump(dict_label_mapping, file_des, ensure_ascii=False, indent=4)
+    print(f"Label mapping is saved to {file_json}")
+    return
+
+
+def main():
+    dir_train = "/home/abhishek/Desktop/datasets/overhead_mnist/version2/train"
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--dir_train",
+        default=dir_train,
+        type=str,
+        help="full directory path to dataset containing training images",
+    )
+    ARGS, unparsed = parser.parse_known_args()
+    gen_label_mapping_json(ARGS)
+    return
+
+
+if __name__ == "__main__":
+    main()
