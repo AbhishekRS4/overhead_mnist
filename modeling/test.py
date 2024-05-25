4b6ae95ae1a9beb03c8546a47521784ad4a0db40
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2024-05-25 10:11:44 +0200

    updated test script

--- modeling/test.py
+++ modeling/test.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 
 from train import validate
 from dataset import get_dataloader_for_testing
-from models import SimpleCNN, SimpleResNet, ComplexResNet, ComplexResNetV2
+from models import SimpleCNN, SimpleResNet, ComplexResNet, ComplexResNetV2, SimpleResKANet, ComplexResKANet, ComplexResKANetV2
 
 
 def test_classifier(ARGS: argparse.Namespace) -> None:
@@ -57,6 +57,24 @@ def test_classifier(ARGS: argparse.Namespace) -> None:
         )
     elif ARGS.model_type == "complex_resnet_v2":
         model = ComplexResNetV2(
+            list_num_res_units_per_block=[4, 4, 4], num_classes=num_classes
+        )
+    elif ARGS.model_type == "simple_reskanet":
+        model = SimpleResKANet(num_classes=num_classes)
+    elif ARGS.model_type == "medium_simple_reskanet":
+        model = SimpleResKANet(
+            list_num_res_units_per_block=[4, 4], num_classes=num_classes
+        )
+    elif ARGS.model_type == "deep_simple_reskanet":
+        model = SimpleResKANet(
+            list_num_res_units_per_block=[6, 6], num_classes=num_classes
+        )
+    elif ARGS.model_type == "complex_reskanet":
+        model = ComplexResKANet(
+            list_num_res_units_per_block=[4, 4, 4], num_classes=num_classes
+        )
+    elif ARGS.model_type == "complex_reskanet_v2":
+        model = ComplexResKANetV2(
             list_num_res_units_per_block=[4, 4, 4], num_classes=num_classes
         )
     else:
