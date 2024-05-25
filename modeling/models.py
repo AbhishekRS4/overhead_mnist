6d74737006b0a1f4aed0acf352ca5f022f2afef7
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2024-05-25 10:08:12 +0200

    added model variants with KAN

--- modeling/models.py
+++ modeling/models.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from typing import List
+from kan import KANLayer
 
 
 class SimpleCNN(nn.Module):
@@ -519,4 +520,416 @@ class ComplexResNetV2(nn.Module):
         x = self.avg_pool(x)
         x = torch.flatten(x, 1)
         x = self.linear(x)
+        return x
+
+
+class SimpleResKANet(nn.Module):
+    def __init__(
+        self,
+        num_classes: int = 10,
+        list_num_res_units_per_block: List = [3, 3],
+        device: str = "cuda",
+    ):
+        """
+        ---------
+        Arguments
+        ---------
+        num_classes: int
+            an integer indicating the number of classes in the dataset
+        list_num_res_units_per_block: list
+            a list of integers representing number of residual units per block
+        """
+        super().__init__()
+        self.list_num_res_units_per_block = list_num_res_units_per_block
+
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(32),
+            nn.ELU(inplace=True),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(32),
+            nn.ELU(inplace=True),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(32),
+            nn.ELU(inplace=True),
+        )
+
+        self.residual_block_1 = nn.Sequential(
+            *[
+                ResidualBlock(32, 32)
+                for i in range(self.list_num_res_units_per_block[0])
+            ]
+        )
+
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(64),
+            nn.ELU(inplace=True),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(64),
+            nn.ELU(inplace=True),
+        )
+
+        self.residual_block_2 = nn.Sequential(
+            *[
+                ResidualBlock(64, 64)
+                for i in range(self.list_num_res_units_per_block[1])
+            ]
+        )
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.kan_layer = KANLayer(64, num_classes, num=10, device=device)
+
+        self._init_weights()
+
+    def _init_weights(self) -> None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+        return
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv_block_1(x)
+        x = self.residual_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.residual_block_2(x)
+        x = self.avg_pool(x)
+        x = torch.flatten(x, 1)
+        x, _, _, _ = self.kan_layer(x)
+        return x
+
+
+class ComplexResKANet(nn.Module):
+    def __init__(
+        self,
+        num_classes: int = 10,
+        list_num_res_units_per_block: List = [4, 4, 4],
+        device: str = "cuda",
+    ):
+        """
+        ---------
+        Arguments
+        ---------
+        num_classes: int
+            an integer indicating the number of classes in the dataset
+        list_num_res_units_per_block: list
+            a list of integers representing number of residual units per block
+        """
+        super().__init__()
+        self.list_num_res_units_per_block = list_num_res_units_per_block
+
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(32),
+            nn.ELU(inplace=True),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(32),
+            nn.ELU(inplace=True),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(32),
+            nn.ELU(inplace=True),
+        )
+
+        self.residual_block_1 = nn.Sequential(
+            *[
+                ResidualBlock(32, 32)
+                for i in range(self.list_num_res_units_per_block[0])
+            ]
+        )
+
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(64),
+            nn.ELU(inplace=True),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(64),
+            nn.ELU(inplace=True),
+        )
+
+        self.residual_block_2 = nn.Sequential(
+            *[
+                ResidualBlock(64, 64)
+                for i in range(self.list_num_res_units_per_block[1])
+            ]
+        )
+
+        self.conv_block_3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(128),
+            nn.ELU(inplace=True),
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(128),
+            nn.ELU(inplace=True),
+        )
+
+        self.residual_block_3 = nn.Sequential(
+            *[
+                ResidualBlock(128, 128)
+                for i in range(self.list_num_res_units_per_block[2])
+            ]
+        )
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.kan_layer = KANLayer(128, num_classes, num=10, device=device)
+
+        self._init_weights()
+
+    def _init_weights(self) -> None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+        return
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv_block_1(x)
+        x = self.residual_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.residual_block_2(x)
+        x = self.conv_block_3(x)
+        x = self.residual_block_3(x)
+        x = self.avg_pool(x)
+        x = torch.flatten(x, 1)
+        x, _, _, _ = self.kan_layer(x)
+        return x
+
+
+class ComplexResKANetV2(nn.Module):
+    def __init__(
+        self,
+        num_classes: int = 10,
+        list_num_res_units_per_block: List = [4, 4, 4],
+        device: str = "cuda",
+    ):
+        """
+        ---------
+        Arguments
+        ---------
+        num_classes: int
+            an integer indicating the number of classes in the dataset
+        list_num_res_units_per_block: list
+            a list of integers representing number of residual units per block
+        """
+        super().__init__()
+        self.list_num_res_units_per_block = list_num_res_units_per_block
+
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=32,
+                kernel_size=5,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(32),
+            nn.ELU(inplace=True),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(32),
+            nn.ELU(inplace=True),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(32),
+            nn.ELU(inplace=True),
+        )
+
+        self.residual_block_1 = nn.Sequential(
+            *[
+                ResidualBlock(32, 32)
+                for i in range(self.list_num_res_units_per_block[0])
+            ]
+        )
+
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(64),
+            nn.ELU(inplace=True),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(64),
+            nn.ELU(inplace=True),
+        )
+
+        self.residual_block_2 = nn.Sequential(
+            *[
+                ResidualBlock(64, 64)
+                for i in range(self.list_num_res_units_per_block[1])
+            ]
+        )
+
+        self.conv_block_3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(128),
+            nn.ELU(inplace=True),
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding="same",
+                bias=False,
+            ),
+            nn.BatchNorm2d(128),
+            nn.ELU(inplace=True),
+        )
+
+        self.residual_block_3 = nn.Sequential(
+            *[
+                ResidualBlock(128, 128)
+                for i in range(self.list_num_res_units_per_block[2])
+            ]
+        )
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.kan_layer = KANLayer(128, num_classes, num=10, device=device)
+
+        self._init_weights()
+
+    def _init_weights(self) -> None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+        return
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv_block_1(x)
+        x = self.residual_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.residual_block_2(x)
+        x = self.conv_block_3(x)
+        x = self.residual_block_3(x)
+        x = self.avg_pool(x)
+        x = torch.flatten(x, 1)
+        x, _, _, _ = self.kan_layer(x)
         return x
