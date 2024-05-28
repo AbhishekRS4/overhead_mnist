4e54d41712ff42c6d0799022b23371af3286f9c8
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2024-05-28 09:25:03 +0200

    updated sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -11,22 +11,21 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-project = 'OverHeadMNIST'
-copyright = '2024, AbhishekRS4'
-author = 'AbhishekRS4'
+project = "OverHeadMNIST"
+copyright = "2024, AbhishekRS4"
+author = "AbhishekRS4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
